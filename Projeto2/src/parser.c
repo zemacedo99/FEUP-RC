f83a058aa60d63bf6181ba89a036e80e450470e8
Zé Macedo <up201705226@fe.up.pt>
2022-01-20 10:02:59 +0000

    parse user pass and host

--- Projeto2/src/parser.c
+++ Projeto2/src/parser.c
@@ -25,13 +25,70 @@ int parse_url(const char* url, ftp_url* args){
         return -1;
     }
 
+    // parsing the user, password and host
+    if(parse_url_first_part(user_pass_host, args) != 0) 
+    {
+        return -1;
+    }
+
     // save the path in the struct
     strcpy(args->path, path);
-
-    strcpy(args->host, "ftp.up.pt");
-
-    // TODO: parse the user, password and host
 
     return 0;
 
 } 
+
+
+
+int parse_url_first_part(char* user_pass_host, ftp_url* args){
+    
+    // storing the first_part to confirm latter
+    char first_part[63+63+255];
+    strcpy(first_part, user_pass_host);
+
+    char *user = strtok(user_pass_host, ":");
+
+    if(user == NULL) 
+    {            
+        printf("Wrong URL format\n");
+        return -1;
+    }
+
+    // no user if the ':' doesn't exists in the initial phrase
+    if(strcmp(user, first_part) == 0)
+    {         
+        // save the user in the struct 
+        strcpy(args->user, "anonymous");
+        // save the pass in the struct 
+        strcpy(args->pass, "");
+        // save the host in the struct 
+        strcpy(args->host, user);
+    } 
+    else 
+    {                
+        // save the user in the struct                   
+        strcpy(args->user, user);
+
+        char *pass = strtok(NULL, "@");
+
+        if(pass == NULL)
+        {            
+            printf("Wrong URL format\n");
+            return -1;
+        }   
+
+        // save the pass in the struct 
+        strcpy(args->pass, pass);
+        
+        char *host = strtok(NULL, "");
+        if(user == NULL)
+        {            
+            printf("Wrong URL format\n");
+            return -1;
+        }
+        
+        // save the host in the struct 
+        strcpy(args->host, host);
+    }
+    return 0;
+}
