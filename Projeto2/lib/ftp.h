9625358067a1347dea592d2c4b88f7684c6ffdb5
Zé Macedo <up201705226@fe.up.pt>
2022-01-26 17:20:26 +0000

    organize folders

--- Projeto2/lib/ftp.h
+++ Projeto2/lib/ftp.h
@@ -13,5 +13,11 @@
 #include "macros.h"
 
 int file_transfer(ftp_url args);
+int receive_server_response(int sockfd, ftp_server_response* response);
+int receive_server_response_string(int sockfd, char* response);
+int send_command(int sockfd, char* command);
+int authenticate_user(int socketfd ,char* user, char* pass);
+int enter_pasv_mode(int socketfd);
+int retr_file(int socketfd, int pasv_socketfd, char* path);
 
 #endif 
--- Projeto2/lib/parser.h
+++ Projeto2/lib/parser.h
@@ -8,5 +8,8 @@
 #include "macros.h"
 
 int parse_url(const char* url, ftp_url* args);
+int parse_url_first_part(char* user_pass_host, ftp_url* args);
+int parse_server_response(const char* response_string, ftp_server_response* response, const char* delimiter);
+int parse_filename(char* path, char* filename);
 
 #endif
